978042fd104f933af3621973b8c1f297837718c4
Chrisploo <christian.petitlaurent@mail.udp.cl>
2026-05-21 19:33:53 -0400

    Create Exploracion-De-Variables.js

---
+++ Encargos/Encargo-06/Exploracion-De-Variables.js
@@ -0,0 +1,24 @@
+function setup() {
+  createCanvas(700, 700);
+  angleMode(DEGREES);
+  frameRate(60);
+}
+
+function draw() {
+  colorMode(RGB);
+  background(31,50,100);
+  
+
+
+  fill(random(180, 255), 30, random(30,200));
+  strokeWeight(0);
+  circle (mouseX, mouseY, random(90,200),)
+
+  
+}
+
+
+
+
+
+
